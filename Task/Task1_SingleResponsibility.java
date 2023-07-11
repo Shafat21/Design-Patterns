02334a4fb990454a36666bcb093b65a0a74ff1df
Shafat Alam <Shafat.mahtab@gmail.com>
2023-07-11 22:35:22 +0600

    Update Task1_SingleResponsibility.java

--- Task/Task1_SingleResponsibility.java
+++ Task/Task1_SingleResponsibility.java
@@ -1,16 +1,23 @@
-public class User {
-    public void login(String username, String password) {
-        //System.out.printf("Login Service");
-    }
-
+public class Task1_SingleResponsibility {
     public static void main(String[] args) {
         EmailService emailService = new EmailService();
-        emailService.sendEmail("TEST", "TEST", "TEST"); 
+        emailService.sendEmail(null,null,null); 
+
+        User loginService = new User();
+        loginService.login(null,null); 
+    }
+}
+class User{
+        public void login(String username, String password) 
+        {
+        System.out.printf("Login Service \n");
     }
 }
 
-class EmailService {
-    public void sendEmail(String to, String subject, String body) {
-        System.out.printf("Email Service");
+class EmailService 
+{
+    public void sendEmail(String to, String subject, String body) 
+    {
+        System.out.printf("Email Service \n");
     }
 }
