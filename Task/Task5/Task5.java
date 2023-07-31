54556ffb6fd5bb0b51aa49e523be33a8f4579081
Shafat Alam <Shafat.mahtab@gmail.com>
2023-07-31 20:31:45 +0600

    Create Task5.java

---
+++ Task/Task5/Task5.java
@@ -0,0 +1,43 @@
+interface INotification {
+    void Notify();
+}
+
+class Notification {
+    private INotification notification;
+
+    public Notification(INotification notification) {
+        this.notification = notification;
+    }
+
+    public void Notify() {
+        notification.Notify();
+    }
+}
+
+class MsgNotification implements INotification {
+    @Override
+    public void Notify() {
+        System.out.println("Msg Notification");
+    }
+}
+
+class CallNotification implements INotification {
+    @Override
+    public void Notify() {
+        System.out.println("Call Notification");
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        MsgNotification msgNotification = new MsgNotification();
+        CallNotification callNotification = new CallNotification();
+
+        Notification msgNotify = new Notification(msgNotification);
+        Notification callNotify = new Notification(callNotification);
+
+        msgNotify.Notify();
+        System.out.println("");
+        callNotify.Notify();
+    }
+}
