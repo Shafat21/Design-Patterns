8a28cb0f829e77b4af7ddefe3f0ecf47b6d6a11f
Shafat Alam <Shafat.mahtab@gmail.com>
2023-07-31 20:13:24 +0600

    Create Task4_InterfaceSegregation.java

---
+++ Task/Task4_InterfaceSegregation.java
@@ -0,0 +1,93 @@
+interface Vehicle
+{
+    void setPrice(double price);
+}
+
+interface Movable
+{
+    void start();
+    void stop();
+}
+
+interface Flyable
+{
+    void fly();
+}
+
+class Car implements Vehicle, Movable
+{
+    double price;
+
+    public void setPrice(double price)
+    {
+        this.price = price;
+        System.out.println("Price:" +price);
+    }
+
+    public void start()
+    {
+        System.out.println("Start");
+    }
+
+    public void stop() 
+    {
+        System.out.println("Stop");
+    }
+}
+
+class Aeroplane implements Vehicle, Movable, Flyable
+{
+    double price;
+
+    public void setPrice(double price)
+    {
+        this.price = price;
+        System.out.println("Price:" +price);
+    }
+
+    public void start()
+    {
+        System.out.println("Start");
+    }
+
+    public void fly()
+    {
+        System.out.println("Fly");
+    }
+
+    public void stop() 
+    {
+        System.out.println("Stop");
+    }
+}
+
+class VehicleBuilder
+{
+    public static Car buildCar()
+    {
+        Car car = new Car();
+        car.setPrice(15.00);
+        car.start();
+        return car;
+    }
+    public static Aeroplane buildAeroplace()
+    {
+        Aeroplane aeroplane = new Aeroplane();
+        aeroplane.setPrice(25.00);
+        aeroplane.start();
+        aeroplane.fly();
+        return aeroplane;
+    }
+}
+
+class Main
+{
+    public static void main(String[] args) 
+    {
+        Car car = VehicleBuilder.buildCar();
+        car.stop();
+
+        Aeroplane aeroplane = VehicleBuilder.buildAeroplace();
+        aeroplane.stop();
+    }
+}
