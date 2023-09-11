52477d7925b13bdddb515aadf023df45cd932d80
Shafat Alam <61838416+Shafat21@users.noreply.github.com>
2023-09-11 21:08:49 +0600

    Create Task_8_Facade_Pattern.java

---
+++ Task/Task_8_Facade_Pattern.java
@@ -0,0 +1,67 @@
+class CPU {
+    public void start() {
+        System.out.println("CPU started");
+    }
+
+    public void shutdown() {
+        System.out.println("CPU shutdown");
+    }
+}
+
+class Memory {
+    public void load() {
+        System.out.println("Memory loaded");
+    }
+
+    public void unload() {
+        System.out.println("Memory unloaded");
+    }
+}
+
+class HardDrive {
+    public void read() {
+        System.out.println("Hard Drive read");
+    }
+
+    public void write() {
+        System.out.println("Hard Drive write");
+    }
+}
+
+class ComputerFacade {
+    private CPU cpu;
+    private Memory memory;
+    private HardDrive hardDrive;
+
+    public ComputerFacade() {
+        cpu = new CPU();
+        memory = new Memory();
+        hardDrive = new HardDrive();
+    }
+
+    public void startComputer() {
+        cpu.start();
+        memory.load();
+        hardDrive.read();
+        System.out.println("Computer started");
+    }
+
+    public void shutdownComputer() {
+        cpu.shutdown();
+        memory.unload();
+        hardDrive.write();
+        System.out.println("Computer shutdown");
+    }
+}
+
+public class FacadePatternDemo {
+    public static void main(String[] args) {
+        ComputerFacade computerFacade = new ComputerFacade();
+
+        System.out.println("----- Starting the computer -----");
+        computerFacade.startComputer();
+
+        System.out.println("\n----- Shutting down the computer -----");
+        computerFacade.shutdownComputer();
+    }
+}
